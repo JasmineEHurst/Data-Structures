77efd1c8cc30164a40edbc0348e15b0c7fc41144
Jasmine Hurst <jasmine.ehurst@gmail.com>
2016-06-01 18:33:02 -0500

    Updated merging method

--- MergeSortedLists/src/MergeLists.java
+++ MergeSortedLists/src/MergeLists.java
@@ -19,9 +19,29 @@ public class MergeLists {
 	}
 
 	private static ListNode<Integer> mergeLinkedLists(ListNode<Integer> l1, ListNode<Integer> l2){
-		ListNode<Integer> placeholder = new ListNode<Integer>(0, null);
+		if(l1 == null) return l2;
+		if(l2 == null) return l1;
 		
-		return placeholder;
+		ListNode<Integer> placeholder = new ListNode<Integer>(0, null);
+		ListNode<Integer> current;
+		current = placeholder;
+		/* Create temporary lists to protect original lists*/
+		ListNode<Integer> list1 = l1, list2 = l2;
+
+		while(list1.next != null && list2.next!=null){
+			if(list1.data <= list2.data){
+				current.next = list1;
+				list1 = list1.next;
+			}else{
+				current.next = list2;
+				list2 = list2.next;
+			}
+			/* Transition full list to the next node*/	
+			current = current.next;
+		}
+		/*Append the rest of the nodes of the longer list*/
+		current.next = list1 != null ? list1 : list2;
+		return placeholder.next;
 	}
 	
 }
